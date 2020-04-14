545bffc39dc6ca439d78fcc07b04834b0af58c2c
hanseji <59394775+hanseji@users.noreply.github.com>
2020-04-14 13:30:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   agent.handleRequest(intentMap);
 });
 
-restService.listen(process.env.PORT || 8080, function() {
-  console.log("Server up and listening");
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
